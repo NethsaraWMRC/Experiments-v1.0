3c45f46b6fc2e6f7319f87cbba0b2e7958ba8ce8
Ravindu Nethsara <139568202+NethsaraWMRC@users.noreply.github.com>
2025-12-12 22:47:19 +0530

    change the start

--- ThreadExperiment/SingleThread.java
+++ ThreadExperiment/SingleThread.java
@@ -1,7 +1,7 @@
 public class SingleThread {
     public SingleThread() {
         long startTime = System.currentTimeMillis();
-        Main.cpuIntensiveTask(0, 100_000_000L);
+        Main.cpuIntensiveTask(1, 100_000_000L);
         long endTime = System.currentTimeMillis();
         System.out.println("SingleThread Execution Time: " + (endTime - startTime) + " ms");
     }
